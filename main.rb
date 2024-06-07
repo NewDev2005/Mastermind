92a3ddf416df67410baadca1e444e336388fafc8
krypto <clayjeansen3@gmail.com>
2024-06-08 00:31:08 +0530

    error generate for invalid input

--- main.rb
+++ main.rb
@@ -1,7 +1,14 @@
+require 'colorize'
 require_relative 'classes/game_board'
+require_relative 'classes/computer'
+require_relative 'classes/player'
 
 board = GameBoard.new
+computer = Computer.new
+human_player = Player.new
 board.display_board
+
+valid_input = [:black,:red,:green,:yellow,:magenta,:cyan,:white,:blue]
 count = 1
 until count == 11
     
@@ -11,12 +18,22 @@ until count == 11
     end
 
     for i in (x...y)
-        user_input = gets.chomp.to_sym
-        board.pegs[i] = "\u25CF".colorize(user_input)
+        human_player.colorChoice = gets.chomp.to_sym
+
+        until valid_input.include?(human_player.colorChoice)
+            puts "Sorry INVALID INPUT/COLOR".colorize(:red)
+            human_player.colorChoice = gets.chomp.to_sym
+        end
+
+        board.pegs[i] = "\u25CF".colorize(human_player.colorChoice)
+        human_player.pegsColor.push(human_player.colorChoice)
     end
+    p human_player.pegsColor #for debug
     x = y 
     y += 4
     board.display_board
+    break if computer.color == human_player.pegsColor
+    human_player.del_pegs_color
     count += 1
 end
  
