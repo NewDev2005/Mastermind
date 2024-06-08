d6d605d9f882647c00f2581ee28ca34f083f8518
krypto <clayjeansen3@gmail.com>
2024-06-08 23:52:25 +0530

    update spacing

--- classes/computer.rb
+++ classes/computer.rb
@@ -1,24 +1,25 @@
 class Computer
-    attr_reader :color
+    attr_reader :colorCode
     def initialize
-        @color = []
+        @colorCode = []
     end
 
     def code_maker()
         count = 0
         while count < 4
             colors_avilable = [:black,:red,:green,:yellow,:magenta,:cyan,:white,:blue]
-            @color.push(colors_avilable[rand(0..7)])
+            @colorCode.push(colors_avilable[rand(0..7)])
             count += 1
         end
-        p @color
     end
 
     def del_previous_color()
         for i in (0..4)
-            @color.pop
+            @colorCode.pop
         end
     end
 
 end
+
+
 
