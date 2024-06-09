ba97bcce7ba2eb7fba032ed0d85e67ebb3a098a1
krypto <clayjeansen3@gmail.com>
2024-06-09 19:57:39 +0530

    feedback on each guess algorithm improved

--- main.rb
+++ main.rb
@@ -15,6 +15,7 @@ human_player = Player.new
 computer.code_maker
 board.display_board
 
+placeholder = computer.colorCode.clone
 valid_input = [:black,:red,:green,:yellow,:magenta,:cyan,:white,:blue]
 count = 1
 until count == 11
@@ -25,8 +26,8 @@ until count == 11
         j = 0
     end
 
-    for i in (x...y)
-        human_player.colorChoice = gets.chomp.downcase.to_sym
+    for i in (x...y)       #players input
+        human_player.colorChoice = gets.chomp.downcase.to_sym 
 
         until valid_input.include?(human_player.colorChoice)
             puts "Sorry INVALID INPUT/COLOR".colorize(:red)
@@ -37,22 +38,43 @@ until count == 11
         human_player.pegsColor.push(human_player.colorChoice)
     end
 
-    break puts "WOAH YOU WON!" if computer.colorCode == human_player.pegsColor
+    #feedback on each guess!
+    if computer.colorCode == human_player.pegsColor
+       for i in (0..3)
+        board.hintPegs[j] = "\u2547".colorize(:red)
+        j += 1
+       end
+        board.display_board
+        puts "CONGRATS YOU WON!".colorize(:green)
+        break
+    end
     
+    for i in (0..3)
+        if computer.colorCode[i] == human_player.pegsColor[i]
+            board.hintPegs[j] = "\u2547".colorize(:red)
+            computer.colorCode[i] = 0
+            human_player.pegsColor[i] = 1
+            j += 1
+        end
+    end
 
     human_player.pegsColor.each_with_index do |human_color, human_index|
         computer.colorCode.each_with_index do |comp_color,comp_index| 
             if human_color == comp_color && human_index == comp_index
                board.hintPegs[j] = "\u2547".colorize(:red)
                j += 1
+               computer.colorCode[comp_index] = 0
+               break
             elsif human_color == comp_color && human_index != comp_index
                 board.hintPegs[j] = "\u2547".colorize(:white)
                 j += 1
+                computer.colorCode[comp_index] = 0
+               break
             end
         end
     end
-    
-    p human_player.pegsColor #for debug
+    computer.colorCode = placeholder.clone
+    # p human_player.pegsColor #for debug
     j = y
     x = y 
     y += 4
@@ -61,4 +83,4 @@ until count == 11
     count += 1
 end
 
-puts p "This was computer's code #{computer.colorCode}"
+ puts "This was computer's code #{computer.colorCode}"
